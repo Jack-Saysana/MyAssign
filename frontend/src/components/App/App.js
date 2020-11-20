1ca60bf66b0188c65ce09a1b305ce1f6f4ee1d73
DasonJ <jandadas000@hsestudents.org>
2020-11-20 08:04:17 -0600

    Navbar

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from '../Home/Home.js';
 
 function App() {
     return (
       <div className="App">
         <h1>MyAssign</h1>
+        <Home/>
       </div>
   );
 }
