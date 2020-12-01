e04efab3c605e7dbc16bffe56c9fd4fa51b570a9
DasonJ <jandadas000@hsestudents.org>
2020-12-01 08:37:21 -0600

    Navbar updates
    Moved to index.html and style.css and improved.

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './Home.css';
 
+
+
 function Home(){
   return(
-    <div className="navbar">
-    <a href="">About</a>
-    <a href="">Calender</a>
-    <a href="">Lists</a>
-    <a href="">Account</a>
-    </div>
+<div>
+</div>
   );
 }
 
