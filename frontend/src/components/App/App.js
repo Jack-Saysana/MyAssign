188ab4ea3aae2aa92239534214ca7c8f90019efc
DasonJ <jandadas000@hsestudents.org>
2020-12-08 08:06:32 -0600

    Fixed Navbar

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from '../Home/Home.js';
+import Navbar from '../Navbar/Navbar.js';
 
 function App() {
     return (
       <div className="App">
         <h1>MyAssign</h1>
+        <Navbar/>
         <Home/>
       </div>
   );
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component }from 'react';
+import './Navbar.css';
 
-class Home extends Component {
+class Navbar extends Component {
   render() {
     return  (
       <div>
@@ -8,14 +9,16 @@ class Home extends Component {
           <script src="https://kit.fontawesome.com/3227043e4d.js" crossorigin="anonymous"></script>
         </head>
 
-        <div class="navbar">
+        <body class="navbar">
           <a href="#"><i class="fas fa-home"></i>Home</a>
           <a href="#"><i class="fas fa-question"></i>About</a>
           <a href="#"><i class="fas fa-calendar"></i>Calender</a>
           <a href="#"><i class="fas fa-list"></i>Lists</a>
           <a href="#"><i class="fas fa-user"></i>Account</a>
-        </div>
+        </body>
       </div>
     );
   }
 }
+
+export default Navbar;
