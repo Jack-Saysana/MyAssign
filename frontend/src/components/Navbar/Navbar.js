27fd576980e28e9941a1d8bab970055824ee063e
Otto-Lutz <72801024+Otto-Lutz@users.noreply.github.com>
2021-01-06 08:00:01 -0500

    navbar css

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component }from 'react';
 import './Navbar.css';
-import ScriptTag from 'react-script-tag';
+//import ScriptTag from 'react-script-tag';
 
 /*Dason Janda*/
 
@@ -8,6 +8,22 @@ class Navbar extends Component {
   render() {
     return  (
       <div >
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+        <head>
+          
+        </head>
+
+        <body class="navbar">
+          <a href="#"><i class="fas fa-home"></i>Home</a>
+          <a href="#"><i class="fas fa-question"></i>About</a>
+          <a href="#"><i class="fas fa-calendar"></i>Calender</a>
+          <a href="#"><i class="fas fa-list"></i>Lists</a>
+          <a href="#"><i class="fas fa-user"></i>Sign In</a>
+        </body>
+=======
+>>>>>>> Stashed changes
         <div>
           <ScriptTag src="https://kit.fontawesome.com/3227043e4d.js" crossOrigin="anonymous" />
         </div>
@@ -18,6 +34,10 @@ class Navbar extends Component {
           <a href="#"><i className="fas fa-list"></i>Lists</a>
           <a href="#"><i className="fas fa-user"></i>Sign In</a>
         </div>
+<<<<<<< Updated upstream
+=======
+>>>>>>> 599acbca860398bce0f04f311b606909fe9ad883
+>>>>>>> Stashed changes
       </div>
     );
   }
