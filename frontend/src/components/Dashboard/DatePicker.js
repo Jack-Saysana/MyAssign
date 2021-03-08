ca3ef5990dd2559bc0c94c561eb13f35017ff40c
Jack Saysana <58755205+Jack-Saysana@users.noreply.github.com>
2021-03-08 11:07:22 -0500

    Calendar Implementation

---
+++ frontend/src/components/Dashboard/DatePicker.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+export default class DatePicker extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            day: "",
+            month: "",
+            year: ""
+        }
+
+        this.convertMonth = this.convertMonth.bind(this);
+        this.changeMonth = this.changeMonth.bind(this);
+        this.monthData = this.monthData.bind(this);
+    }
+
+    componentDidMount(){
+        const date = new Date()
+        this.setState({
+            day: date.getDate(),
+            month: date.getMonth(),
+            year: date.getFullYear()
+        })
+    }
+
+    convertMonth(month) {
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        return months[month];
+    }
+
+    changeMonth(event) {
+        event.preventDefault();
+        const month = event.target.name === "back" ? this.state.month -= 1 : this.state.month += 1;
+        this.setState({
+            month: month > 11 ? 0 : month < 0 ? 11 : month,
+            year: month > 11 ? this.state.year += 1 : month < 0 ? this.state.year -= 1 : this.state.year
+        });
+    }
+
+    monthData(year, month){
+        const numDays = new Date(year, month + 1, 0).getDate();
+        const firstDay = new Date(year, month).getDay();
+        const numRows = Math.ceil((numDays - (7 - firstDay))/7) + 1;
+        const monthData = [];
+        for(let i = 0; i < 7*numRows; i++){
+            const day = i < firstDay ? new Date(year, month, -(firstDay - 1 - i)) : new Date(year, month, (i - firstDay + 1));
+            monthData.push({
+                day: day.getDate(),
+                month: day.getMonth(),
+                year: day.getFullYear()
+            });
+        }
+        return monthData;
+    }
+    
+    render(){
+        return(
+            <div>
+                <div className="date-picker">
+                    <button name="back" onClick={this.changeMonth}>Back</button>
+                    <h1>{this.convertMonth(this.state.month)}</h1>
+                    <h2>{this.state.year}</h2>
+                    <button name="forward" onClick={this.changeMonth}>Forward</button>
+                    <div className="calendar">
+                        {this.monthData(this.state.year, this.state.month).map(day => 
+                            <div className="day">{day.day}</div>    
+                        )}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- frontend/src/components/Dashboard/Folder.js
+++ frontend/src/components/Dashboard/Folder.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React from 'react';
-import AssignmentAdd from './AssignmentAdd';
+import DatePicker from './DatePicker';
 
 //Jack Saysana
 
@@ -8,9 +8,16 @@ export default class Folder extends React.Component {
     constructor(props){
         super(props)
         this.state={
-            assignments: []
+            assignments: [],
+            title: "",
+            notes: "",
+            due: null
         }
-        this.closeAnnoField = this.closeAnnoField.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.changeDate = this.changeDate.bind(this);
+        this.enterListen = this.enterListen.bind(this);
+        this.toggleAnnoField = this.toggleAnnoField.bind(this);
     }
 
     async componentDidMount(){
@@ -20,14 +27,51 @@ export default class Folder extends React.Component {
             })
         });
     }
-    
-    closeAnnoField(event){
+
+    async handleSubmit() {
+        if (this.state.title !== "") {
+            await axios.post('http://localhost:5000/assignment', {
+                _id: sessionStorage.getItem('user'),
+                folder: this.props.id,
+                title: this.state.title,
+                notes: this.state.notes,
+                due: this.state.due
+            })
+            await axios.get(`http://localhost:5000/assignments/${sessionStorage.getItem('user')}/${this.props.id}`).then(res => {
+                this.setState({
+                    assignment: res.data.assignments
+                })
+            })
+        }
+    }
+
+    handleChange(event) {
         event.preventDefault();
-        document.getElementsByClassName("annotations-field")[this.props.index].style.display = "";
-        document.getElementsByClassName("annotations-field")[this.props.index].style.marginTop = "";
-        document.getElementsByClassName("folder")[this.props.index].style.paddingBottom = "";
-        document.getElementsByClassName("annotations")[this.props.index].style.visibility = "";
-        document.getElementsByClassName("add-assignment")[this.props.index].style.borderBottom = "";
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    changeDate(date){
+        this.setState({
+            due: date
+        })
+    }
+
+    enterListen(event) {
+        if (event.key === "Enter") {
+            this.handleSubmit();
+        }
+    }
+
+    toggleAnnoField(event){
+        event.preventDefault();
+        document.getElementsByClassName("annotations-field")[this.props.index].style.display = document.getElementsByClassName("annotations-field")[this.props.index].style.display === "block" ? "" : "block";
+        document.getElementsByClassName("annotations-field")[this.props.index].style.marginTop = document.getElementsByClassName("annotations-field")[this.props.index].style.marginTop === "3rem" ? "" : "3rem";
+        document.getElementsByClassName("folder")[this.props.index].style.paddingBottom = document.getElementsByClassName("folder")[this.props.index].style.paddingBottom === "0rem" ? "" : "0rem";
+        document.getElementsByClassName("annotations")[this.props.index].style.visibility = document.getElementsByClassName("annotations")[this.props.index].style.visibility === "visible" ? "" : "visible";
+        document.getElementsByClassName("add-assignment")[this.props.index].style.borderBottom = document.getElementsByClassName("annotations")[this.props.index].style.visibility === "visible" ? "2px solid #443ab0" : "";
+        document.getElementsByClassName("add-assignment")[this.props.index].focus();
     }
 
     render(){
@@ -42,13 +86,18 @@ export default class Folder extends React.Component {
                     <p>test</p>
                     <p>test</p>
                     <p>test</p>
-                    <AssignmentAdd folder={this.props.id} index={this.props.index} />
+                    <div>
+                        <input className="add-assignment add-elem" type="text" name="title" placeholder="New Assignment" autoComplete="off" onBlur={this.toggleAnnotations} onKeyDown={this.enterListen} onChange={this.handleChange} ></input>
+                        <div className="add-assignment-button add-elem-button" onClick={this.handleSubmit} />
+                        <div className="annotations">
+                            <div className="add-due" onClick={this.toggleAnnoField} />
+                            <div className="add-notes" onClick={this.toggleAnnoField} />
+                        </div>
+                    </div>
                     <div className="annotations-field">
-                        <button onClick={this.closeAnnoField}>close</button>
-                        <p>test</p>
-                        <p>test</p>
-                        <p>test</p>
-                        <p>test</p>
+                        <div className="close" onClick={this.toggleAnnoField} />
+                        <textarea className="notes-input" name="notes" placeholder="Notes..." onChange={this.handleChange} />
+                        <DatePicker changeDate={this.changeDate} />
                     </div>
                 </ul>
             </div>
